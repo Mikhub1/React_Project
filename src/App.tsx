513d8350e302b77efd3932ee85d19bf6beae8673
Mikhub1 <122497460+Mikhub1@users.noreply.github.com>
2024-05-09 10:45:17 -0230

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import "./App.css"
 import sun from './images/sun.jpg'
 import Form from './Form';
 import React from "react"
+import { useEffect } from 'react';
 
 
 function App(){
+
+  useEffect(() => {
+    document.title = "Weather App"
+})    
 
   return (
     <div>
