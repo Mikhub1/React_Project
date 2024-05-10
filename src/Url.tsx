0814229551b2188c96459fcc4489f3a56a2dc221
Mikhub1 <122497460+Mikhub1@users.noreply.github.com>
2024-05-10 19:56:27 -0230

    Add files via upload

--- src/Url.tsx
+++ src/Url.tsx
@@ -1,17 +1,14 @@
-  import city from "./Form";
+  import "./Form";
   
   /**
   */
-interface Props{
-    city: string
-} 
 
-function Generate_Url(city: Props){
+function Generate_Url(){
     
-    const base_id = "http://api.openweathermap.org/data/2.5/weather?"
+    const base_id = "http://api.openweathermap.org/data/2.5/weather/"
     const api_key = "a1d832c101f04481e98ece5a0a6cb290"
 
-    const Complete_Url = base_id + "appid="+api_key+"&q=" + city
+    const Complete_Url = base_id + "appid="+api_key+"&q="
 
     return (Complete_Url)
 
